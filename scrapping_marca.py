3bf5e39fd193da81612a4c81ac1dfbf1567b99c7
lmcarvajal96 <63042569+lmcarvajal96@users.noreply.github.com>
2021-01-31 20:12:18 +0100

    Update scrapping_marca.py

--- scrapping_marca.py
+++ scrapping_marca.py
@@ -15,8 +15,32 @@ for eq in soup.find_all('h3', class_="mod-title"):
     noticias = m.group(1)
     lista_noticias.append(noticias)
     
-for noticia in lista_noticias:
-    data_noticias['noticias'] = noticia
-    data_noticias.append(data_noticias)
+df = pd.DataFrame(lista_noticias)
 
-print(data_noticias)
+project_id = 'news_MARCA'
+
+from google.colab import auth
+auth.authenticate_user()
+
+from googleapiclient.discovery import build
+gcs_service = build('storage', 'v1')
+bucket_name = 'segmento_noticias'
+
+file_name = 'noticias.csv'
+
+from googleapiclient.http import MediaFileUpload
+
+media = MediaFileUpload(df, 
+                        mimetype='text/plain',
+                        resumable=True)
+
+request = gcs_service.objects().insert(bucket=bucket_name, 
+                                       name=file_name,
+                                       media_body=media)
+
+response = None
+while response is None:
+ 
+  _, response = request.next_chunk()
+
+print('Upload completo')
